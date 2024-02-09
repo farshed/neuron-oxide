44f3e2102f0bdb1b2f97819b0fd0aacbaf1caf5e
farshed <faisalarshed28@gmail.com>
2024-02-10 02:41:31 +0500

    Add go, js and py implementations

---
+++ index.js
@@ -0,0 +1,65 @@
+const data = require('./data.json');
+
+class NeuralNetwork {
+	constructor() {
+		this.weights = [Math.random(), Math.random()];
+		this.bias = Math.random();
+		this.learningRate = 0.1;
+	}
+
+	predict(input) {
+		const sum = this.weights.reduce((acc, weight, i) => {
+			acc += input[i] * weight;
+			return acc;
+		}, this.bias);
+
+		return sigmoid(sum);
+	}
+
+	train(inputs, outputs, epochs) {
+		for (let i = 0; i < epochs; i++) {
+			inputs.forEach((input, j) => {
+				const output = this.predict(input);
+
+				const error = outputs[j] - output;
+				const gradient = derivative(output);
+
+				this.weights.forEach((_, k) => {
+					this.weights[k] += this.learningRate * error * input[k] * gradient;
+				});
+
+				this.bias += this.learningRate * error * gradient;
+			});
+		}
+	}
+}
+
+const sigmoid = (x) => 1 / (1 + Math.exp(-x));
+const derivative = (x) => x * (1 - x);
+
+// Train and test
+
+const inputs = data.training_inputs;
+const outputs = data.training_outputs;
+
+const neuralNet = new NeuralNetwork();
+neuralNet.train(inputs, outputs, 1e5);
+
+const testInputs = data.test_inputs;
+
+let correct = 0;
+
+for (const input of testInputs) {
+	const prediction = neuralNet.predict(input);
+	const actual = input[0] + input[1];
+
+	// if (Math.abs(prediction - actual) <= 0.1) {
+	if (prediction.toFixed(1) === actual.toFixed(1)) {
+		correct++;
+	}
+	console.log(`Input: [${input}], Prediction: ${prediction.toFixed(1)}`);
+}
+
+const accuracy = ((correct * 100) / testInputs.length).toFixed(1);
+
+console.log(`${correct} / ${testInputs.length} correct. Accuracy: ${accuracy}%`);
---
+++ main.py
@@ -0,0 +1,81 @@
+import random
+
+with open('data.json') as data_file:
+  data = data_file.read()
+
+class NeuralNet:
+  def __init__(self):
+    self.weights = [random.random(), random.random()]
+    self.bias = random.random()
+    self.learning_rate = 0.1
+
+  def predict(self, _input):
+    _sum = self.bias
+    for weight in self.weights:
+      _sum += _input[]
+  
+  const data = require('./data.json');
+
+class NeuralNetwork {
+	constructor() {
+		this.weights = [Math.random(), Math.random()];
+		this.bias = Math.random();
+		this.learningRate = 0.1;
+	}
+
+	predict(input) {
+		const sum = this.weights.reduce((acc, weight, i) => {
+			acc += input[i] * weight;
+			return acc;
+		}, this.bias);
+
+		return sigmoid(sum);
+	}
+
+	train(inputs, outputs, epochs) {
+		for (let i = 0; i < epochs; i++) {
+			inputs.forEach((input, j) => {
+				const output = this.predict(input);
+
+				const error = outputs[j] - output;
+				const delta = derivative(output);
+
+				this.weights.forEach((_, k) => {
+					this.weights[k] += this.learningRate * error * input[k] * delta;
+				});
+
+				this.bias += this.learningRate * error * delta;
+			});
+		}
+	}
+}
+
+const sigmoid = (x) => 1 / (1 + Math.exp(-x));
+const derivative = (x) => x * (1 - x);
+
+// Train and test
+
+const inputs = data.training_inputs;
+const outputs = data.training_outputs;
+
+const neuralNet = new NeuralNetwork();
+neuralNet.train(inputs, outputs, 1e5);
+
+const testInputs = data.test_inputs;
+
+let correct = 0;
+
+for (const input of testInputs) {
+	const prediction = neuralNet.predict(input);
+	const actual = input[0] + input[1];
+
+	// if (Math.abs(prediction - actual) <= 0.1) {
+	if (prediction.toFixed(1) === actual.toFixed(1)) {
+		correct++;
+	}
+	console.log(`Input: [${input}], Prediction: ${prediction.toFixed(1)}`);
+}
+
+const accuracy = ((correct * 100) / testInputs.length).toFixed(1);
+
+console.log(`${correct} / ${testInputs.length} correct. Accuracy: ${accuracy}%`);
